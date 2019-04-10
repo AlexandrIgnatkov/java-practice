1e1801858b4b38cc6d67e9c91301beed942b4ddb
AlexandrIgnatkov <49477636+AlexandrIgnatkov@users.noreply.github.com>
2019-04-10 14:56:54 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         String cat="Барсик";
-        System.out.println("Hello World!" + "\n" + cat);
+        System.out.println("Hello World!\n" + cat);
     }
 }

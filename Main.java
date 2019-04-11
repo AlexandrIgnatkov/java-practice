1a7fa3c2ccb623605846a4fccd840786592503cc
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-04-11 21:22:09 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         String cat="Барсик";
+        
+        // "\n" - зачем?
         System.out.println("Hello World!\n" + cat);
     }
 }

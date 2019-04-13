5cad193ec5dc89746b21ba7955f47fa9cabd5f9d
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-04-13 23:14:06 +0300

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,5 +1,9 @@
 package practice;
 public class Program {
+    
+    // Называй пожалуйста джава файлы соот-но номеру задания
+    // например, public class Program -> public class Task_1
+    
     public static void main(String[] args) {
         Bang bang = new Bang();
         bang.hello();

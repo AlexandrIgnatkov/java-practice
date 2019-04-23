43d70cf603eb1d6fd77726a424699a49ccd13fa3
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-04-23 22:26:16 +0300

    Update Task3.java

--- hw_3/task_3/Task3.java
+++ hw_3/task_3/Task3.java
@@ -3,6 +3,8 @@ package practice.hw_3.task_3;
 public class Task3 {
     public static void main(String[] args) {
         String dog = "Whara is my dog+";
+        
+        // можно в одну строку dog.replace('a','e').replace('+','?')
         String repDog1 = dog.replace('a','e');
         String repDog2 = repDog1.replace('+','?');
         System.out.println(repDog2);

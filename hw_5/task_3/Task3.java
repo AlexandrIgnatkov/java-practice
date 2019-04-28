0e23faab774e42f4c0f5c0038b6fd7f98991e1dc
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-04-28 23:43:26 +0300

    Update Task3.java

--- hw_5/task_3/Task3.java
+++ hw_5/task_3/Task3.java
@@ -4,12 +4,17 @@ public class Task3 {
 
     public static void main(String[] args) {
         int[] myArray = {12, 32, 3432, 232, 312};
+        
+            // что такое n? 
         int n = myArray.length;
+        
+          // что такое i? называй переменные как положено
         int i =0;
         System.out.println(n);
         index(i,n,myArray);
     }
 
+    // что такое i? w? 
     static void index(int i, int w, int[] array) {
         if (w>0){
             w--;
@@ -19,5 +24,5 @@ public class Task3 {
         }
     }
 
-
+// не должно быть таких пустых строк
 }

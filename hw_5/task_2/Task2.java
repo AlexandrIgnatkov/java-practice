9e340e85f4a2ff1cc172d2c9a219adb87e9a8b90
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-04-28 23:40:47 +0300

    Update Task2.java

--- hw_5/task_2/Task2.java
+++ hw_5/task_2/Task2.java
@@ -10,7 +10,15 @@ public class Task2 {
         metod(args, wordToFind);
     }
 
+    // не стоит называть метод методом -> check, contains - имя должно говорить что метод делает
+
+    //слово method пишется с буквой H
     static void metod(String[] args1, String find) {
+        // да, но решение не оптимально
+        
+        /* Arrays.asList(args1); - вернет тебе ArrayList, а если ты посмотришь в реализацию метода contains у ArrayList,
+        то увидишь что он бежит по всему листу и ищет совпадение. Тоже самое и нужно сделать тебе, а не создавать доп лист
+        */
         List valid = Arrays.asList(args1);
         if (valid.contains(find)) {
             System.out.println("true");;

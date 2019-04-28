f29bf70373c0609ae676a1b66f1eaf7c19cf0e4b
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-04-28 23:34:33 +0300

    Update Task1.java

--- hw_5/task_1/Task1.java
+++ hw_5/task_1/Task1.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
+        // хорошо что разделил на два метода, но метод shar() лучше пусть позвращает boolean и затем в методе мейн вывод
+        
+        // имена методов надо писать по-англ, привыкай. 
         vvod();
         shar();
     }
@@ -14,6 +17,8 @@ public class Task1 {
         String question;
         question = sc.next();
         while (question.charAt(question.length()-1) != '?' ){
+            
+            // думаю стоит намекнуть пользователю про знак вопроса
             System.out.println("Задайте вопрос еще раз");
             question = sc.next();
         }

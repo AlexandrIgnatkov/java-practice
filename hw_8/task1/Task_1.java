dc247645ee57986a619a7ae5d2f585b5011c97af
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-05-20 17:20:37 +0300

    Update Task_1.java

--- hw_8/task1/Task_1.java
+++ hw_8/task1/Task_1.java
@@ -3,16 +3,22 @@ package practice.hw_8.task1;
 public class Task_1 {
     public static void main(String[] args) {
         Input input = new Input();
+        
         System.out.print("Введите дизайн дома:\n1 - Стандартный;\n2 - Хайтек.\n");
         int design = input.inputHosue();
+        
         System.out.println("Введите этажность дома: ");
         int floor = input.inputHosue();
+        
         System.out.println("Наличие гаража?\n1 - да;\n2 - нет.");
         int garage = input.inputHosue();
+        
         System.out.println("Наличие забора?\n1 - да;\n2 - нет.");
         int fence = input.inputHosue();
+       
         SelectHouse selectHouse = new SelectHouse(floor, design, garage, fence);
         selectHouse.printHouse();
     }
 }
 
+  

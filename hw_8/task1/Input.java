f275bb62a8ab66e1ae91591ef5ac45704224053e
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-05-20 17:23:28 +0300

    Update Input.java

--- hw_8/task1/Input.java
+++ hw_8/task1/Input.java
@@ -2,11 +2,17 @@ package practice.hw_8.task1;
 
 import java.util.Scanner;
 
+/* думаю целый класс не нужен. + ты каждый раз создаешь     
+Scanner scan = new Scanner(System.in);
+когда вызываешь метод, это очень плохо, одного раза должно быть достаточно
+*/
+
 public class Input {
    // имя метода с опечаткой
    public int inputHosue(){
        boolean condition = true;
        int num = 0;
+      // while(true) - ты все равно из цикла через break выходишь, переменная condition не нужна
        while (condition) {
            Scanner scan = new Scanner(System.in);
            try { num = scan.nextInt();

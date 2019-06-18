222d3c4c9fe953d315ec450617bb555725ba12a5
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-06-18 12:27:42 +0300

    Update TextInput.java

--- hw_12/task1/TextInput.java
+++ hw_12/task1/TextInput.java
@@ -2,12 +2,14 @@ package practice.hw_12.task1;
 
 import java.io.*;
 
+// ты вычитал файл и что-то вывел на экран. В задании говорилось про изменения в файле, т.е. надо отформатировать текст
 public class TextInput {
     public static void main(String[] args) {
         int i=0;
         final String PATH = "C:\\Users\\agent\\IdeaProjects\\Practice\\src\\practice\\hw_12\\task1\\task1.txt";
         try (BufferedReader br = new BufferedReader(new FileReader(PATH))) {
             int textRead;
+            // br.readLine() используй
             while ((textRead = br.read()) != -1) {
                 if (textRead == 32){
                     System.out.print(",");
